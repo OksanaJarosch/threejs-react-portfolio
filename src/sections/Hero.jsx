fa811919c478b5bfe7ff0c372405cd470fc8acc2
Oksana Jarosch <ok.jarosch@gmail.com>
2024-10-25 15:11:23 +0200

    Hero section with Canvas and CanvasLoader

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,33 @@
+import {Canvas} from "@react-three/fiber";
+import {PerspectiveCamera} from "@react-three/drei";
+import HackerRoom from "../components/HackerRoom.jsx";
+import {Suspense} from "react";
+import CanvasLoader from "../components/CanvasLoader.jsx";
+
 const Hero = () => {
     return (
-        <div>Hero</div>
+        <section className="min-h-screen w-full flex flex-col relative">
+            <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+                <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">Hi, I am Oksana <span className="waving-hand">👋🏻</span></p>
+                <p className="hero_tag text-gray_gradient">Building Products & Brands</p>
+            </div>
+
+            <div className="w-full h-full absolute inset-0">
+                <Canvas className="w-full h-full">
+                    <Suspense fallback={<CanvasLoader />}>
+                        <PerspectiveCamera makeDefault position={[0, 0, 30]}/>
+                        <HackerRoom
+                            position={[0, 0, 0]}
+                            scale={0.05}
+                            rotation={[0, 280, 0]}
+                        />
+                        <ambientLight intensity={1}/>
+                        <directionalLight position={[10, 10, 10]} intensity={0.5}/>
+                    </Suspense>
+                </Canvas>
+            </div>
+        </section>
     )
-}
+};
+
 export default Hero;
