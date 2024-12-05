43a30c0709756f2be48074996b2616ec47f40a95
Oksana Jarosch <ok.jarosch@gmail.com>
2024-12-05 12:49:35 +0100

    update/section about

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -21,15 +21,16 @@ const About = () => {
             <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
 
                 <div className="col-span-1 xl:row-span-3">
-                    <div className="grid-container">
-                        <img src="/assets/grid1.png" alt="default photo" className="w-full sm:h-[276px] h-fit object-contain" />
+                    <div className="grid-container relative">
+                        <img src="/assets/first_grid.png" alt="default photo" className="absolute left-0 w-full sm:h-[276px] h-fit object-contain" />
+                        <img src="/assets/my_photo.png" alt="developer photo" className="  relative z-10 w-full sm:h-[276px] h-fit object-contain" />
 
                         <div>
                             <p className="grid-headtext">
                                 Hi, I&apos;m Oksana
                             </p>
                             <p className="grid-subtext">
-                                I have been coding for one year, during which I have honed my frontend skills, with a focus on animated 3D websites.
+                                I have been coding for one year, during which I have honed my frontend skills, with a focus on single-page applications.
                             </p>
                         </div>
                     </div>
@@ -37,7 +38,7 @@ const About = () => {
 
                 <div className="col-span-1 xl:row-span-3">
                     <div className="grid-container">
-                        <img src="/assets/grid2.png" alt="logos" className="w-full sm:h-[276px] h-fit object-contain"/>
+                        <img src="/assets/second_grid.png" alt="logos" className="w-full sm:h-[276px] h-fit object-contain"/>
 
                         <div>
                             <p className="grid-headtext">
@@ -52,7 +53,7 @@ const About = () => {
 
 
                 <div className="col-span-1 xl:row-span-4">
-                    <div className="grid-container">
+                    <div className="grid-container xl:justify-around">
                         <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
                             <Globe
                                 height={326}
@@ -77,11 +78,11 @@ const About = () => {
 
                 <div className="xl:col-span-2 xl:row-span-3">
                     <div className="grid-container">
-                        <img src="/assets/grid3.png" alt="grid" className="w-full sm:h-[266px] h-fit object-contain"/>
+                        <img src="/assets/grid3.png" alt="grid" className="w-full md:h-[328px] sm:h-[266px] h-fit object-contain"/>
 
                         <div>
                             <p className="grid-headtext">Problem solving</p>
-                            <p className="grid-subtext">I love solving problems through code.</p>
+                            <p className="grid-subtext">Solving problems through code is not just a task for me but an opportunity to combine creativity and logic to create real value.</p>
                         </div>
                     </div>
                 </div>
@@ -89,14 +90,14 @@ const About = () => {
 
                 <div className="col-span-1 xl:row-span-2">
                     <div className="grid-container">
-                        <img src="/assets/grid4.png" alt="numbers" className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top"/>
+                        <img src="/assets/grid4.png" alt="numbers" className="w-full md:h-[196px] sm:h-[276px] h-fit object-cover sm:object-top"/>
 
                         <div className="space-y-2">
                             <p className="grid-subtext text-center">Contact me</p>
 
                             <div className="copy-container" onClick={handleCopy}>
                                 <img src={hasCopied ? 'assets/tick.svg' : 'assets/copy.svg'} alt="copy"/>
-                                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient">ok.jarosch@gmail.com</p>
+                                <p className="text-xl font-medium text-gray_gradient">ok.jarosch@gmail.com</p>
                             </div>
                         </div>
                     </div>
